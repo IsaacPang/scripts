82ec41e4ec0dda7eaf83e72d676ab953ede32f51
Isaac Pang <ispang@deloitte.com.au>
2023-03-12 17:46:21 +1100

    update create ts project

--- bash/create_ts_project_mac_bsd.sh
+++ bash/create_ts_project_mac_bsd.sh
@@ -1,6 +1,17 @@
-# adapted from blog text
-# https://khalilstemmler.com/blogs/typescript/node-starter-project/
-# note that this works for BSD/macOS, as sed has different implementations
+#!/usr/bin/env bash
+# adapted from blog text https://khalilstemmler.com/blogs/typescript/node-starter-project/
+# USAGE:
+#       Execute file to create a typescript projec with basic installations and configs, including:
+#               1. nodemon      - for dev cold reloading
+#               2. rimraf       - for production builds
+#               3. jest         - for testing
+#
+# OPTIONS:
+#       -n      Sets folder and project name
+#
+# NOTES:
+#       1. This is not portable: only works for BSD/macOS, as sed has different implementations
+#       2. Remember to chmod +x <this_file_name> to ensure executability
 
 # retrieve cli inputs
 name=""
@@ -28,15 +39,15 @@ npm i typescript --save-dev
 npm i @types/node --save-dev
 
 # create tsconfig.json
-npx tsc --init \
-        --rootDir src \                 # set root directory to ./src/
-        --outDir build \                # set build directory to ./build/
-        --esModuleInterop \             # set commonjs intermodules to true
-        --resolveJsonModule \           # if we use json as modules, set to true
-        --lib es6 \                     # use es6 compiled down to target es5
-        --module commonjs \             # use commonjs as the standard module system
-        --allowJs true \                # allow js compatibility
-        --noImplicitAny true            # no implicit any types allowed, all must be specified
+# set root directory to ./src/
+# set build directory to ./build/
+# set commonjs intermodules to true
+# if we use json as modules, set to true
+# use es6 compiled down to target es5
+# use commonjs as the standard module system
+# allow js compatibility
+# no implicit any types allowed, all must be specified
+npx tsc --init --rootDir src --outDir build --esModuleInterop --resolveJsonModule --lib es6 --module commonjs --allowJs true --noImplicitAny true
 
 # delete all purely commented files in tsconfig
 sed -i '' -E -e '\#^[ \t]*/\*|^[ \t]*//|^[ \t]*$#d' tsconfig.json  # delete lines starting with /*, // and blank lines
@@ -47,7 +58,7 @@ sed -i '' -e 's/[ \t]*$//g' tsconfig.json                          # remove all 
 mkdir src
 touch src/index.ts
 
-# cold reloading
+# cold reloading using nodemon and ts-node
 npm i nodemon ts-node --save-dev
 echo '{' > nodemon.json
 sed -i '' -e '$ a\
@@ -69,5 +80,11 @@ sed -i '' -e '/npx nodemon/ a\
     "build": "rimraf ./build && tsc",' package.json
 
 # production startup script
-sed -i '' -e '/rimraf/ a\
+sed -i '' -e '/"rimraf .\/build && tsc"/ a\
     "start": "npm run build && node build/index.js",' package.json
+
+# tests
+npm i jest --save-dev
+sed -i '' -e '/"test": /d' package.json
+sed -i '' -e '/npm run build/ a\
+    "test": "jest"' package.json
